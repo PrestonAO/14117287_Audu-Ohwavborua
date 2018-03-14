b653aa9f1dea31d9eb9e02cfc86ed0558bddeee6
Preston <pa14aav@herts.ac.uk>
2018-03-14 12:42:02 +0000

    Included the collision between the main car and the obstacle cars

--- CarGame2/GameViewController.swift
+++ CarGame2/GameViewController.swift
@@ -14,7 +14,9 @@ class GameViewController: UIViewController {
     
     var dynamicAnimator: UIDynamicAnimator!
     var dynamicItemBehaviour: UIDynamicItemBehavior!
-
+    var gravityBehaviour: UIGravityBehavior!
+    var collisionBehaviour: UICollisionBehavior!
+    
     @IBOutlet weak var RoadA: UIImageView!
     @IBOutlet weak var CarView: DraggedImage!
     @IBOutlet weak var ObstacleView: UIImageView!
@@ -28,20 +30,24 @@ class GameViewController: UIViewController {
         dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
         
         dynamicItemBehaviour = UIDynamicItemBehavior(items: [ObstacleView])
-        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 300), for: ObstacleView)
+        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 100), for: ObstacleView)
         dynamicAnimator.addBehavior(dynamicItemBehaviour)
-        
         dynamicItemBehaviour = UIDynamicItemBehavior(items: [ObstacleView1])
-        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 300), for: ObstacleView1)
+        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 200), for: ObstacleView1)
         dynamicAnimator.addBehavior(dynamicItemBehaviour)
-        
         dynamicItemBehaviour = UIDynamicItemBehavior(items: [ObstacleView2])
-        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 300), for: ObstacleView2)
+        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 100), for: ObstacleView2)
+        dynamicAnimator.addBehavior(dynamicItemBehaviour)
+        dynamicItemBehaviour = UIDynamicItemBehavior(items: [ObstacleView3])
+        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 200), for: ObstacleView3)
         dynamicAnimator.addBehavior(dynamicItemBehaviour)
         
-        dynamicItemBehaviour = UIDynamicItemBehavior(items: [ObstacleView3])
-        self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 300), for: ObstacleView3)
-        dynamicAnimator.addBehavior(dynamicItemBehaviour)
+        gravityBehaviour = UIGravityBehavior(items: [ObstacleView, ObstacleView1, ObstacleView2, ObstacleView3])
+        dynamicAnimator.addBehavior(gravityBehaviour)
+        
+        collisionBehaviour = UICollisionBehavior(items: [ObstacleView, ObstacleView1, ObstacleView2, ObstacleView3])
+        collisionBehaviour.translatesReferenceBoundsIntoBoundary = true
+        dynamicAnimator.addBehavior(collisionBehaviour)
         
         self.view.addSubview(CarView)
         self.view.bringSubview(toFront: CarView)
@@ -59,7 +65,7 @@ class GameViewController: UIViewController {
         
          imagearray = [UIImage(named: "road1.png")!,UIImage(named: "road2.png")!,UIImage(named: "road3.png")!,UIImage(named: "road4.png")!,UIImage(named: "road5.png")!,UIImage(named: "road6.png")!,UIImage(named: "road7.png")!,UIImage(named: "road8.png")!,UIImage(named: "road9.png")!,UIImage(named: "road10.png")!,UIImage(named: "road11.png")!,UIImage(named: "road12.png")!,UIImage(named: "road13.png")!,UIImage(named: "road14.png")!,UIImage(named: "road15.png")!,UIImage(named: "road16.png")!,UIImage(named: "road17.png")!,UIImage(named: "road18.png")!,UIImage(named: "road19.png")!,UIImage(named: "road20.png")!]
         
-        RoadA.image = UIImage.animatedImage(with: imagearray, duration: 1)
+        RoadA.image = UIImage.animatedImage(with: imagearray, duration: 0.2)
     }
 
     override func didReceiveMemoryWarning() {
