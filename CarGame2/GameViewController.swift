66265b490d16c961615a3b668c609d3c20a0dd1c
Preston <pa14aav@herts.ac.uk>
2018-04-18 21:46:07 +0100

    implemented the score calculations and also the replay function

--- CarGame2/GameViewController.swift
+++ CarGame2/GameViewController.swift
@@ -16,6 +16,7 @@ protocol subviewDelegate {
 
 class GameViewController: UIViewController, subviewDelegate {
     
+    @IBOutlet weak var restartB: UIButton!
     @IBOutlet weak var GameScore: UILabel!
     var carAnimator: UIDynamicAnimator!
     var dynamicItemBehaviour: UIDynamicItemBehavior!
@@ -25,24 +26,26 @@ class GameViewController: UIViewController, subviewDelegate {
     var ScoreArray: [UIImageView] = []
     var gameCars: [UIImageView] = []
     let GameOver = UIImageView(image: nil)
-    let Rbutton = UIButton(frame: CGRect(x: 150, y: 80, width: 100, height: 150))
     
     let randomCarArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     
     @IBOutlet weak var RoadA: UIImageView!
     @IBOutlet weak var CarView: DraggedImage!
     
-    
-    @objc func restart (sender: UIButton!) {
-        GameScore.removeFromSuperview()
-        Rbutton.removeFromSuperview()
+    @IBAction func replayB(_ sender: Any) {
+        viewDidLoad()
+        
+        GameScore.text = ""
         Score = 0;
+        GameOver.isHidden = true
+        restartB.isHidden = true
+        
         
         for i in gameCars {
             i.removeFromSuperview()
         }
+        viewDidLoad()
     }
-    
     
     func changeSomething()
     {
@@ -61,8 +64,14 @@ class GameViewController: UIViewController, subviewDelegate {
     override func viewDidLoad() {
           super.viewDidLoad()
         
+        var imagearray: [UIImage]
+        imagearray = [UIImage(named: "road1.png")!,UIImage(named: "road2.png")!,UIImage(named: "road3.png")!,UIImage(named: "road4.png")!,UIImage(named: "road5.png")!,UIImage(named: "road6.png")!,UIImage(named: "road7.png")!,UIImage(named: "road8.png")!,UIImage(named: "road9.png")!,UIImage(named: "road10.png")!,UIImage(named: "road11.png")!,UIImage(named: "road12.png")!,UIImage(named: "road13.png")!,UIImage(named: "road14.png")!,UIImage(named: "road15.png")!,UIImage(named: "road16.png")!,UIImage(named: "road17.png")!,UIImage(named: "road18.png")!,UIImage(named: "road19.png")!,UIImage(named: "road20.png")!]
+        
+        RoadA.image = UIImage.animatedImage(with: imagearray, duration: 0.2)
+        
+        
         CarView.myDelegate = self
-    
+        
         self.view.addSubview(CarView)
         self.view.bringSubview(toFront: CarView)
         
@@ -70,43 +79,40 @@ class GameViewController: UIViewController, subviewDelegate {
         dynamicItemBehaviour = UIDynamicItemBehavior(items:[])
         collisionBehaviour = UICollisionBehavior(items:[])
         
-        
-        
-        
         for index in 0...19 {
             
             let delay = Double(self.randomCarArray[index])
             
             let when = DispatchTime.now() + delay
             
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            
-            let Obstacle = arc4random_uniform(6)
-            let ObstacleView = UIImageView(image: nil)
-            let screenWidth = UIScreen.main.bounds.width
+            DispatchQueue.main.asyncAfter(deadline: when) {
                 
-            switch Obstacle {
+                let Obstacle = arc4random_uniform(6)
+                let ObstacleView = UIImageView(image: nil)
+                let screenWidth = UIScreen.main.bounds.width
+                
+                switch Obstacle {
                 case 1: ObstacleView.image = UIImage(named: "car3.png")
                 case 2: ObstacleView.image = UIImage(named: "carp.png")
                 case 3: ObstacleView.image = UIImage(named: "car6.png")
                 case 4: ObstacleView.image = UIImage(named: "carx.png")
-                
+                    
                 default:
                     ObstacleView.image = UIImage(named: "car3.png")
                 }
                 
                 ObstacleView.frame = CGRect(x: Int(arc4random_uniform(UInt32(screenWidth))), y: 0, width: 40, height:65 )
                 
-            self.view.addSubview(ObstacleView)
-            self.view.bringSubview(toFront: ObstacleView)
+                self.view.addSubview(ObstacleView)
+                self.view.bringSubview(toFront: ObstacleView)
                 
-            self.dynamicItemBehaviour.addItem(ObstacleView)
-            self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 300), for: ObstacleView)
-            self.collisionBehaviour.addItem(ObstacleView)
-            
-            self.ScoreArray.append((ObstacleView))
-            self.Score += 2
-            self.GameScore.text = String(self.Score)
+                self.dynamicItemBehaviour.addItem(ObstacleView)
+                self.dynamicItemBehaviour.addLinearVelocity(CGPoint(x: 0, y: 300), for: ObstacleView)
+                self.collisionBehaviour.addItem(ObstacleView)
+                
+                self.ScoreArray.append((ObstacleView))
+                self.Score += 2
+                self.GameScore.text = String(self.Score)
             }
         }
         
@@ -115,23 +121,20 @@ class GameViewController: UIViewController, subviewDelegate {
         collisionBehaviour.translatesReferenceBoundsIntoBoundary = false
         carAnimator.addBehavior(collisionBehaviour)
         
-        var imagearray: [UIImage]
-         imagearray = [UIImage(named: "road1.png")!,UIImage(named: "road2.png")!,UIImage(named: "road3.png")!,UIImage(named: "road4.png")!,UIImage(named: "road5.png")!,UIImage(named: "road6.png")!,UIImage(named: "road7.png")!,UIImage(named: "road8.png")!,UIImage(named: "road9.png")!,UIImage(named: "road10.png")!,UIImage(named: "road11.png")!,UIImage(named: "road12.png")!,UIImage(named: "road13.png")!,UIImage(named: "road14.png")!,UIImage(named: "road15.png")!,UIImage(named: "road16.png")!,UIImage(named: "road17.png")!,UIImage(named: "road18.png")!,UIImage(named: "road19.png")!,UIImage(named: "road20.png")!]
-        
-        RoadA.image = UIImage.animatedImage(with: imagearray, duration: 0.2)
-        
         let endGame = DispatchTime.now() + 20
         DispatchQueue.main.asyncAfter(deadline: endGame) {
             
-            let GameOver = UIImageView(image: UIImage(named: "game_over.jpg"))
-            GameOver.frame = UIScreen.main.bounds
-            self.view.addSubview(GameOver)
-            self.view.bringSubview(toFront: GameOver)
+            self.GameOver.isHidden = false
+            self.restartB.isHidden = false
+            self.GameOver.image = UIImage(named: "game_over.jpg")
+            self.GameOver.frame = UIScreen.main.bounds
+            self.view.addSubview(self.GameOver)
+            self.view.bringSubview(toFront: self.GameOver)
+            self.view.bringSubview(toFront: self.restartB)
             
         }
     }
     
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
